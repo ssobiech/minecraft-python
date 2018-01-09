b9090304990a5aee3f118c4cbf33f5d5c303a559
ssobiech <s.sobiech@gmail.com>
2018-01-09 22:24:15 +0100

    first file
    dotyk Midasa in minecraft

---
+++ dotykMidasa.py
@@ -0,0 +1,18 @@
+import mcpi.minecraft as minecraft
+import time
+
+mc = minecraft.Minecraft.create()
+
+def dotykMidasa():
+    gold = 41
+    water = 9
+    air = 0
+
+    while True:
+	    pos = mc.player.getTilePos()
+	    blockBelow = mc.getBlock(pos.x, pos.y - 1, pos.z)
+	    if blockBelow != air and blockBelow != water:
+   		    mc.setBlock(pos.x, pos.y - 1, pos.z, gold)
+	    time.sleep(0.1)
+
+dotykMidasa()
