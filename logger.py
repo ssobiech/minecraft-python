2adf7fb674cff2c4db949207780f240e31cf6641
ssobiech <s.sobiech@gmail.com>
2018-02-15 20:30:05 +0100

    create logger class

---
+++ logger.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+from logstash_async.handler import AsynchronousLogstashHandler
+from os import environ
+
+host = 'satoh.nazwa.pl'
+port = 6240
+
+class Logger:
+    def __init__(self):
+        self.extra = { 'player': environ["MINECRAFT_PLAYER_NAME"]}
+        self.logger = logging.getLogger('python-logstash-logger')
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(AsynchronousLogstashHandler(host, port, database_path='logstash_test.db'))
+
+    def info(self, data):
+        self.logger.info(data, extra = self.extra)
+
+
+if __name__ == "__main__":
+    environ["MINECRAFT_PLAYER_NAME"] = "Spock"
+    logger = Logger()
+    logger.info("sample log message")
