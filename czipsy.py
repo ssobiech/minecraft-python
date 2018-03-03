8a0619f02f516c71a8b636e6fde1a6dbcbb57af8
msobiech <maks.sobiech@gmail.com>
2018-03-03 20:01:26 +0100

    paraboloida

---
+++ czipsy.py
@@ -0,0 +1,21 @@
+import time
+import serwPol
+import logger
+from random import randint
+from mcpi import block
+
+
+def figura(pos, r, a, b):
+    for i in range(-r, r + 1):
+        for j in range(-r, r + 1):
+            for k in range(-r, r + 1):
+                if ((i ** 2) / 1 ** 2 - (k ** 2) / a ** 2 <= j * 2) and (
+                        (i ** 2) / 1 ** 2 - (k ** 2) / b ** 2 > j * 2 - 10):
+                    mc.setBlock(pos.x + i, pos.y + j, pos.z + k, block.STONE.id, 2)
+
+
+if __name__ == "__main__":
+    mc = serwPol.podlacz()
+    logger = logger.Logger()
+    pos = mc.player.getTilePos()
+    figura(pos, 10, 2, 2)
