8fb611a7b23c2d708502dbece86074a8690a491a
ssobiech <s.sobiech@gmail.com>
2018-03-10 17:11:06 +0100

    poprawa wyboru gracza

--- serwPol.py
+++ serwPol.py
@@ -1,20 +1,29 @@
 import mcpi.minecraft as minecraft
 from os import environ
+import mcpi.connection as conn
+import sys
 import logger
 
-gracz = "souza539"
+gracz = "Spock"
 server ="spock.nazwa.pl"
 port = 4711
-environ["MINECRAFT_PLAYER_ID"] = 'souza539'
+environ["MINECRAFT_PLAYER_NAME"] = gracz
 
 logger = logger.Logger()
 
 
 def polacz():
-    mc = minecraft.Minecraft.create(server, port)
-    logger.info("użytkownik " + mc.player.getName() + " został podłączony do serwera " + server + ":" + str(port))
-    return mc
-
+    try:
+        mc = minecraft.Minecraft.create(server, port)
+        if mc.player.getName() == gracz:
+            logger.info("użytkownik " + mc.player.getName() + " został podłączony do serwera " + server + ":" + str(port))
+            return mc
+        else:
+            '''logger.error("podlaczono, ale brak mozliwosci identyfikacji gracza %s " % gracz)'''
+            raise conn.RequestError("podlaczono do niewlasciwego gracza %s " % mc.player.getName())
+    except conn.RequestError as err:
+        logger.error("problem z podlaczeniem do serwera. Błąd: " + err.__str__())
+        raise
 
 def podlacz():
     mc = polacz()
